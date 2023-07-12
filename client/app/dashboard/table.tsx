9823ec6fb8747c7ebbce1c2dbfd4647ac52473d3
Ricardo <ricoledan@gmail.com>
2023-07-12 01:46:23 -0400

    misc updates

--- client/app/dashboard/table.tsx
+++ client/app/dashboard/table.tsx
@@ -1,12 +1,4 @@
-import {
-  Table,
-  TableHead,
-  TableRow,
-  TableHeaderCell,
-  TableBody,
-  TableCell,
-  Text
-} from '@tremor/react';
+import { Table, TableHead, TableRow, TableHeaderCell, TableBody, TableCell, Text } from '@tremor/react';
 
 interface User {
   din: string;
@@ -20,33 +12,71 @@ interface User {
   date: string;
 }
 
-export default async function ApplicationTable({ users }: { users: User[] }) {
-  return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableHeaderCell>Type</TableHeaderCell>
-          <TableHeaderCell>Status</TableHeaderCell>
-          <TableHeaderCell>Full Name</TableHeaderCell>
-          <TableHeaderCell>Date</TableHeaderCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {users.map((user) => (
-          <TableRow key={user.type}>
-            <TableCell>{user.type}</TableCell>
-            <TableCell>
-              <Text>{user.status}</Text>
-            </TableCell>
-            <TableCell>
-              <Text>{user.fullName}</Text>
-            </TableCell>
-            <TableCell>
-              <Text>{user.date}</Text>
-            </TableCell>
+export default function ApplicationTable({ users }: { users: User[] }) {
+  const currentDate = new Date();
+
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1; // Month is zero-based, so we add 1
+  const day = currentDate.getDate();
+
+  const formattedDate = `${day}/${month}/${year}`;
+
+  let displayValues: any[] = [];
+
+  if (typeof localStorage !== 'undefined') {
+    const storedValues = JSON.parse(localStorage.getItem('formValues') || '[]');
+    displayValues = Array.isArray(storedValues) ? storedValues : [];
+  }
+
+  try {
+    return (
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeaderCell>Type</TableHeaderCell>
+            <TableHeaderCell>Status</TableHeaderCell>
+            <TableHeaderCell>Full Name</TableHeaderCell>
+            <TableHeaderCell>Date</TableHeaderCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  );
+        </TableHead>
+        <TableBody>
+          {users.length > 0 ? (
+            users.map((user) => (
+              <TableRow key={user.type}>
+                <TableCell>{user.type}</TableCell>
+                <TableCell>
+                  <Text>{user.status}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text>{user.fullName}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text>{user.date}</Text>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            displayValues.map((value, index) => (
+              <TableRow key={index}>
+                <TableCell>Driver's License</TableCell>
+                <TableCell>
+                  <Text>Pending</Text>
+                </TableCell>
+                <TableCell>
+                  <Text>{value.fullName}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text>{formattedDate}</Text>
+                </TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+    );
+  } catch (error) {
+    // Handle the error gracefully, e.g., log it or display a fallback UI
+    console.error('Hydration error:', error);
+    return null; // or return a fallback UI component
+  }
 }
--- client/app/form.tsx
+++ client/app/form.tsx
@@ -1,13 +1,22 @@
-import {FormEvent, useState} from 'react';
+import { FormEvent, useState, useEffect } from 'react';
 import Web3 from 'web3';
 
 export default function Home() {
-    const [ssn, setSsn] = useState("");
-    const [fullName, setFullName] = useState("");
-    const [dateOfBirth, setDateOfBirth] = useState("");
-    const [residentialAddress, setResidentialAddress] = useState("");
-    const [error, setError] = useState(""); // State for displaying validation error
+    const [ssn, setSsn] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [dateOfBirth, setDateOfBirth] = useState('');
+    const [residentialAddress, setResidentialAddress] = useState('');
+    const [error, setError] = useState(''); // State for displaying validation error
     const [submitted, setSubmitted] = useState(false); // State for tracking form submission
+    const [formValues, setFormValues] = useState([]); // State for storing form values
+
+    useEffect(() => {
+        // Retrieve form values from local storage
+        const storedFormValues = localStorage.getItem('formValues');
+        if (storedFormValues) {
+            setFormValues(JSON.parse(storedFormValues));
+        }
+    }, []);
 
     const handleSubmit = async (event: FormEvent) => {
         event.preventDefault();
@@ -16,7 +25,7 @@ export default function Home() {
             return;
         }
 
-        // walkthrough this piece
+        // Handle form submission logic here
         try {
             const web3 = new Web3('http://localhost:8545');
             const accounts = await web3.eth.getAccounts();
@@ -34,70 +43,110 @@ export default function Home() {
             console.log(`Gas used: ${receipt.gasUsed}`);
             console.log(`Cumulative gas used: ${receipt.cumulativeGasUsed}`);
             console.log(`Transaction status: ${receipt.status}`);
-            console.log(`Submitted SSN: ${ssn}, Full Name: ${fullName}, Date of Birth: ${dateOfBirth}, Residential Address: ${residentialAddress}`);
+            console.log(
+              `Submitted SSN: ${ssn}, Full Name: ${fullName}, Date of Birth: ${dateOfBirth}, Residential Address: ${residentialAddress}`
+            );
+
+            // Store form values in local storage
+            const formData = { ssn, fullName, dateOfBirth, residentialAddress };
+            const updatedFormValues = [...formValues, formData];
+            // @ts-ignore
+            setFormValues(updatedFormValues);
+            localStorage.setItem('formValues', JSON.stringify(updatedFormValues));
 
             setSubmitted(true); // Update form submission status
-            setError(""); // Clear any previous error messages
+            setError(''); // Clear any previous error messages
         } catch (error) {
             // Handle error during transaction submission
-            setError("An error occurred during form submission.");
+            setError('An error occurred during form submission.');
         }
     };
 
     const validateForm = () => {
         if (!ssn || !fullName || !dateOfBirth || !residentialAddress) {
-            setError("Please fill in all fields.");
+            setError('Please fill in all fields.');
             return false;
         }
         // Add additional validation logic if needed
-        setError("");
+        setError('');
         return true;
     };
 
     return (
-        <div>
-            {submitted ? (
-                <p className="text-green-500 mb-4">Form submitted successfully!</p>
-            ) : (
-                <div>
-                    <h3 className="text-center mb-4">Please fill in the following details:</h3>
-                    <form onSubmit={handleSubmit} className="max-w-xl mx-auto p-4 md:p-8 my-12 rounded-lg shadow-xl">
-                        {error && <p className="text-red-500 mb-4">{error}</p>}
-                        {submitted && (
-                            <p className="text-green-500 mb-4">Form submitted successfully!</p>
-                        )}
-                        <div className="flex flex-col mb-4">
-                            <label htmlFor="ssn" className="mb-2 font-bold text-lg text-grey-darkest">SSN:</label>
-                            <input id="ssn" type="text" value={ssn} onChange={(e) => setSsn(e.target.value)}
-                                   className="border py-2 px-3 text-grey-darkest"/>
-                        </div>
-                        <div className="flex flex-col mb-4">
-                            <label htmlFor="fullName" className="mb-2 font-bold text-lg text-grey-darkest">Full
-                                Name:</label>
-                            <input id="fullName" type="text" value={fullName}
-                                   onChange={(e) => setFullName(e.target.value)}
-                                   className="border py-2 px-3 text-grey-darkest"/>
-                        </div>
-                        <div className="flex flex-col mb-4">
-                            <label htmlFor="dateOfBirth" className="mb-2 font-bold text-lg text-grey-darkest">Date of
-                                Birth:</label>
-                            <input id="dateOfBirth" type="date" value={dateOfBirth}
-                                   onChange={(e) => setDateOfBirth(e.target.value)}
-                                   className="border py-2 px-3 text-grey-darkest"/>
-                        </div>
-                        <div className="flex flex-col mb-4">
-                            <label htmlFor="residentialAddress" className="mb-2 font-bold text-lg text-grey-darkest">Residential
-                                Address:</label>
-                            <input id="residentialAddress" type="text" value={residentialAddress}
-                                   onChange={(e) => setResidentialAddress(e.target.value)}
-                                   className="border py-2 px-3 text-grey-darkest"/>
-                        </div>
-                        <button type="submit"
-                                className="block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit
-                        </button>
-                    </form>
-                </div>
-            )}
-        </div>
+      <div>
+          {submitted ? (
+            <p className="text-green-500 mb-4">Form submitted successfully!</p>
+          ) : (
+            <div>
+                <h3 className="text-center mb-4">Please fill in the following details:</h3>
+                <form
+                  onSubmit={handleSubmit}
+                  className="max-w-xl mx-auto p-4 md:p-8 my-12 rounded-lg shadow-xl"
+                >
+                    {error && <p className="text-red-500 mb-4">{error}</p>}
+                    {submitted && (
+                      <p className="text-green-500 mb-4">Form submitted successfully!</p>
+                    )}
+                    <div className="flex flex-col mb-4">
+                        <label htmlFor="ssn" className="mb-2 font-bold text-lg text-grey-darkest">
+                            SSN:
+                        </label>
+                        <input
+                          id="ssn"
+                          type="text"
+                          value={ssn}
+                          onChange={(e) => setSsn(e.target.value)}
+                          className="border py-2 px-3 text-grey-darkest"
+                        />
+                    </div>
+                    <div className="flex flex-col mb-4">
+                        <label htmlFor="fullName" className="mb-2 font-bold text-lg text-grey-darkest">
+                            Full Name:
+                        </label>
+                        <input
+                          id="fullName"
+                          type="text"
+                          value={fullName}
+                          onChange={(e) => setFullName(e.target.value)}
+                          className="border py-2 px-3 text-grey-darkest"
+                        />
+                    </div>
+                    <div className="flex flex-col mb-4">
+                        <label htmlFor="dateOfBirth" className="mb-2 font-bold text-lg text-grey-darkest">
+                            Date of Birth:
+                        </label>
+                        <input
+                          id="dateOfBirth"
+                          type="date"
+                          value={dateOfBirth}
+                          onChange={(e) => setDateOfBirth(e.target.value)}
+                          className="border py-2 px-3 text-grey-darkest"
+                        />
+                    </div>
+                    <div className="flex flex-col mb-4">
+                        <label
+                          htmlFor="residentialAddress"
+                          className="mb-2 font-bold text-lg text-grey-darkest"
+                        >
+                            Residential Address:
+                        </label>
+                        <input
+                          id="residentialAddress"
+                          type="text"
+                          value={residentialAddress}
+                          onChange={(e) => setResidentialAddress(e.target.value)}
+                          className="border py-2 px-3 text-grey-darkest"
+                        />
+                    </div>
+                    <button
+                      type="submit"
+                      className="block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
+          )}
+      </div>
     );
 }
